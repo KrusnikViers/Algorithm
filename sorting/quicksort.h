c3d8f1391246daa7621aa9ffc783e5b4ad0b029e
Viers <krusnik.viers@gmail.com>
2017-05-01 22:39:09 +0300

    Heapsort added. Fixed quicksort algorithm.

--- sorting/quicksort.h
+++ sorting/quicksort.h
@@ -8,7 +8,7 @@ namespace sort {
 // Three-way splitting quicksort: begin:equal_it - elements, lesser than base, equal_it:greater_it - equal,
 // greater_it:end - greater.
 template<class RandomAccessIterator, class Compare>
-void impl_quicksort(RandomAccessIterator begin, RandomAccessIterator end, Compare comp)
+void impl_quick(RandomAccessIterator begin, RandomAccessIterator end, Compare comp)
 {
     if (end - begin < 2u)
         return;
@@ -17,8 +17,15 @@ void impl_quicksort(RandomAccessIterator begin, RandomAccessIterator end, Compar
     auto base = begin + rand() % (end - begin);
     for (auto it = begin; it != end; ++it) {
         if (comp(*it, *base)) {
-            std::iter_swap(equal_it, greater_it);
-            std::iter_swap(it, equal_it);
+            if (equal_it != greater_it) {
+                if (greater_it != it)
+                    std::iter_swap(equal_it, greater_it);
+                if (equal_it == base)
+                    base = greater_it;
+                std::iter_swap(it, equal_it);
+            } else {
+                std::iter_swap(it, greater_it);
+            }
             ++equal_it;
             ++greater_it;
         } else if (!comp(*base, *it)) {
@@ -28,20 +35,20 @@ void impl_quicksort(RandomAccessIterator begin, RandomAccessIterator end, Compar
             ++greater_it;
         }
     }
-    impl_quicksort(begin, equal_it, comp);
-    impl_quicksort(greater_it, end, comp);
+    impl_quick(begin, equal_it, comp);
+    impl_quick(greater_it, end, comp);
 }
 
 template<class RandomAccessIterator, class Compare>
-void quicksort(RandomAccessIterator begin, RandomAccessIterator end, Compare comp)
+void quick(RandomAccessIterator begin, RandomAccessIterator end, Compare comp)
 {
-    impl_quicksort(begin, end, comp);
+    impl_quick(begin, end, comp);
 }
 
 template<class RandomAccessIterator>
-void quicksort(RandomAccessIterator begin, RandomAccessIterator end)
+void quick(RandomAccessIterator begin, RandomAccessIterator end)
 {
-    return quicksort(begin, end, std::less<typename std::iterator_traits<RandomAccessIterator>::value_type>());
+    return quick(begin, end, std::less<typename std::iterator_traits<RandomAccessIterator>::value_type>());
 }
 
 }  // namespace sort
