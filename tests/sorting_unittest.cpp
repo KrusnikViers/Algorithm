e352b9c9f89c6638783a157709791d7157c58dd0
Viers <krusnik.viers@gmail.com>
2017-05-01 22:39:26 +0300

    Tests for sorting algorithms refactored.

--- tests/sorting_unittest.cpp
+++ tests/sorting_unittest.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "gtest/gtest.h"
+#include "sorting/heapsort.h"
 #include "sorting/mergesort.h"
 #include "sorting/simplesort.h"
 #include "sorting/quicksort.h"
@@ -13,17 +14,36 @@ class SortingTest : public ::testing::Test,
                     public ::testing::WithParamInterface<const char*>
 {
 protected:
+    void startMeasurement()
+    {
+        start_time_ = std::chrono::steady_clock::now();
+    }
+
+    void endMeasurement()
+    {
+        const auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::steady_clock::now() - start_time_).count();
+        std::cout << "Input: " << GetParam() << std::endl;
+        std::cout << "Time: " << duration << " us" << std::endl;
+    }
+
+    std::chrono::steady_clock::time_point start_time_;
+};
+
+// Test stable sorting
+class StableSortingTest : public SortingTest
+{
+protected:
     struct StableNode
     {
         int value;
         int order;
-
         bool operator< (const StableNode& other) const { return value < other.value; }
     };
 
-    void initializeStableDataAndTimer(const char* filename)
+    void prepareSortingTest()
     {
-        std::ifstream input_stream(filename, std::istream::in);
+        std::ifstream input_stream(GetParam(), std::istream::in);
         int N;
         input_stream >> N;
         ASSERT_GT(N, 0);
@@ -34,92 +54,25 @@ protected:
             data[i].order = i;
         }
         input_stream.close();
-        stable_data_.swap(data);
-        start_time_ = std::chrono::steady_clock::now();
+        data_.swap(data);
+        startMeasurement();
     }
 
-    void initializeUsualDataAndTimer(const char* filename)
+    void checkSorting()
     {
-        std::ifstream input_stream(filename, std::istream::in);
-        int N;
-        input_stream >> N;
-        ASSERT_GT(N, 0);
-
-        std::vector<int> data(N);
-        for (int i = 0; i < N; ++i)
-            input_stream >> data[i];
-        input_stream.close();
-        usual_data_.swap(data);
-        start_time_ = std::chrono::steady_clock::now();
-    }
-
-    void checkDataAndTimer(bool is_stable)
-    {
-        std::cout << "Time: " <<
-            std::chrono::duration_cast<std::chrono::microseconds>(
-                std::chrono::steady_clock::now() - start_time_).count() << " us" << std::endl;
-        if (is_stable) {
-            for (size_t i = 1; i < stable_data_.size(); ++i) {
-                if (stable_data_[i - 1].value != stable_data_[i].value)
-                    ASSERT_LT(stable_data_[i - 1].value, stable_data_[i].value) << i;
-                else
-                    ASSERT_LT(stable_data_[i - 1].order, stable_data_[i].order) << i;
-            }
-        } else {
-            for (size_t i = 1; i < stable_data_.size(); ++i)
-                ASSERT_LE(usual_data_[i - 1], usual_data_[i]) << i;
+        endMeasurement();
+        for (size_t i = 1; i < data_.size(); ++i) {
+            if (data_[i - 1].value != data_[i].value)
+                ASSERT_LT(data_[i - 1].value, data_[i].value) << i;
+            else
+                ASSERT_LT(data_[i - 1].order, data_[i].order) << i;
         }
     }
 
-    std::vector<StableNode> stable_data_;
-    std::vector<int>        usual_data_;
-
-    std::chrono::steady_clock::time_point start_time_;
+    std::vector<StableNode> data_;
 };
 
-TEST_P(SortingTest, Bubble)
-{
-    initializeStableDataAndTimer(GetParam());
-    sort::bubble(stable_data_.begin(), stable_data_.end());
-    checkDataAndTimer(true);
-}
-
-TEST_P(SortingTest, Selection)
-{
-    initializeUsualDataAndTimer(GetParam());
-    sort::selection(usual_data_.begin(), usual_data_.end());
-    checkDataAndTimer(false);
-}
-
-TEST_P(SortingTest, StableSelection)
-{
-    initializeStableDataAndTimer(GetParam());
-    sort::stableSelection(stable_data_.begin(), stable_data_.end());
-    checkDataAndTimer(true);
-}
-
-TEST_P(SortingTest, Insertion)
-{
-    initializeStableDataAndTimer(GetParam());
-    sort::insertion(stable_data_.begin(), stable_data_.end());
-    checkDataAndTimer(true);
-}
-
-TEST_P(SortingTest, Merge)
-{
-    initializeStableDataAndTimer(GetParam());
-    sort::merge(stable_data_.begin(), stable_data_.end());
-    checkDataAndTimer(true);
-}
-
-TEST_P(SortingTest, Quicksort)
-{
-    initializeUsualDataAndTimer(GetParam());
-    sort::quicksort(stable_data_.begin(), stable_data_.end());
-    checkDataAndTimer(false);
-}
-
-INSTANTIATE_TEST_CASE_P(DifferentInput, SortingTest,
+INSTANTIATE_TEST_CASE_P(IntegerInput, StableSortingTest,
                         ::testing::Values("data/sorting/all_duplicates.txt",
                                           "data/sorting/highly_dispersed.txt",
                                           "data/sorting/highly_duplicated.txt",
@@ -128,3 +81,119 @@ INSTANTIATE_TEST_CASE_P(DifferentInput, SortingTest,
                                           "data/sorting/unique_1.txt",
                                           "data/sorting/unique_2.txt",
                                           "data/sorting/unique_3.txt"));
+
+TEST_P(StableSortingTest, Bubble)
+{
+    prepareSortingTest();
+    sort::bubble(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(StableSortingTest, StableSelection)
+{
+    prepareSortingTest();
+    sort::stableSelection(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(StableSortingTest, Insertion)
+{
+    prepareSortingTest();
+    sort::insertion(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(StableSortingTest, Merge)
+{
+    prepareSortingTest();
+    sort::merge(data_.begin(), data_.end());
+    checkSorting();
+}
+
+// Test unstable sorting (also suitable for stable algorithms)
+class UnstableSortingTest : public SortingTest
+{
+protected:
+    void prepareSortingTest()
+    {
+        std::ifstream input_stream(GetParam(), std::istream::in);
+        int N;
+        input_stream >> N;
+        ASSERT_GT(N, 0);
+
+        std::vector<int> data(N);
+        for (int i = 0; i < N; ++i)
+            input_stream >> data[i];
+        input_stream.close();
+        data_.swap(data);
+        startMeasurement();
+    }
+
+    void checkSorting()
+    {
+        endMeasurement();
+        for (size_t i = 1; i < data_.size(); ++i)
+            ASSERT_LE(data_[i - 1], data_[i]) << i;
+    }
+
+    std::vector<int> data_;
+};
+
+INSTANTIATE_TEST_CASE_P(IntegerInput, UnstableSortingTest,
+                        ::testing::Values("data/sorting/all_duplicates.txt",
+                                          "data/sorting/highly_dispersed.txt",
+                                          "data/sorting/highly_duplicated.txt",
+                                          "data/sorting/rarely_duplicated.txt",
+                                          "data/sorting/single_number.txt",
+                                          "data/sorting/unique_1.txt",
+                                          "data/sorting/unique_2.txt",
+                                          "data/sorting/unique_3.txt"));
+
+TEST_P(UnstableSortingTest, Bubble)
+{
+    prepareSortingTest();
+    sort::bubble(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(UnstableSortingTest, Selection)
+{
+    prepareSortingTest();
+    sort::selection(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(UnstableSortingTest, StableSelection)
+{
+    prepareSortingTest();
+    sort::stableSelection(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(UnstableSortingTest, Insertion)
+{
+    prepareSortingTest();
+    sort::insertion(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(UnstableSortingTest, Merge)
+{
+    prepareSortingTest();
+    sort::merge(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(UnstableSortingTest, Quicksort)
+{
+    prepareSortingTest();
+    sort::quick(data_.begin(), data_.end());
+    checkSorting();
+}
+
+TEST_P(UnstableSortingTest, Heapsort)
+{
+    prepareSortingTest();
+    sort::heap(data_.begin(), data_.end());
+    checkSorting();
+}
